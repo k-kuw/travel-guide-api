6fbe410c91b8a71fd2aa0c82ad629e17aaa0e960
Kazuaki Kuwahara <kazuaki.kuwahara@rakus-partners.co.jp>
2024-06-13 17:41:59 +0900

    トークン認証処理追加

---
+++ auth.py
@@ -0,0 +1,73 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from fastapi import Depends, HTTPException, status
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordBearer
+from database import get_user
+from config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# パスワード検証処理
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# パスワードハッシュ化処理
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# ユーザ認証処理
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user[2]):
+        return False
+    return user
+
+# トークン作成処理
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
+
+# 現ユーザ情報取得処理
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(name=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
--- main.py
+++ main.py
@@ -1,11 +1,67 @@
-from fastapi import FastAPI
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+from config import settings
+from pydantic import BaseModel
+from auth import Token, User, get_current_user, authenticate_user, create_access_token
 from routers import users, guides
+
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(guides.router)
 
+# CORS対応
+origins = [
+    settings.browser_url
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ログイン認証(トークン)処理
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": user[0]}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# @app.get("/users/me/", response_model=User)
+# async def read_users_me(
+#     current_user: Annotated[User, Depends(get_current_user)],
+# ):
+#     return current_user
+
+# @app.get("/users/me/items/")
+# async def read_own_items(
+#     current_user: Annotated[User, Depends(get_current_user)],
+# ):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
+
+# @app.get("/items/")
+# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+#     return {"token": token}
